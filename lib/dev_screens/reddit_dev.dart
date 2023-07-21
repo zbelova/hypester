30f82270b68541897f4fd0b867df8db94f6522bb
Zoya Belova (macbook) <alirasirin@gmail.com>
2023-07-21 15:10:32 +0300

    Подключила reddit api, сделала вывод списка саббредитов по поисковому запросу (без выбора темы)

--- lib/dev_screens/reddit_dev.dart
+++ lib/dev_screens/reddit_dev.dart
@@ -1,3 +1,4 @@
+import 'package:draw/draw.dart';
 import 'package:flutter/material.dart';
 
 class RedditPage extends StatefulWidget {
@@ -7,7 +8,37 @@ class RedditPage extends StatefulWidget {
   State<RedditPage> createState() => _RedditPageState();
 }
 
+
+
+
 class _RedditPageState extends State<RedditPage> {
+
+List<Subreddit> _subreddits = [];
+
+  void initReddit() async {
+    final userAgent = 'hypester by carrot';
+    final reddit = await Reddit.createUntrustedReadOnlyInstance(
+        userAgent: userAgent,
+        clientId: "LZAPVGNW0N1P_PLTg9argA",
+        //deviceId: "hypester by carrot " + Random.secure().nextInt(999999).toString(),
+        deviceId: "hypester by carrot 1"
+    );
+    reddit.subreddits.search('magic the gathering', limit:10).listen((event) {
+      var data = event as Subreddit;
+      setState(() {
+        _subreddits.add(data);
+      });
+    });
+  }
+
+  @override
+  void initState(){
+    initReddit();
+   // reddit.subreddits.search('magic the gathering').first.then((value) => print(value.toString()));
+    super.initState();
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,9 +46,27 @@ class _RedditPageState extends State<RedditPage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text('Reddit'),
       ),
-      body: const Center(
+      body: Center(
           child:
-          Text('Reddit')
+          Column(
+            children: [
+              Expanded(
+                child: ListView.builder(
+                  itemCount: _subreddits.length,
+                  itemBuilder: (context, index) {
+                    if(_subreddits.isEmpty)
+                      return Text('empty');
+                    else {
+                      return ListTile(
+                      title: Text(_subreddits[index].title),
+                      subtitle: Text(_subreddits[index].displayName),
+                    );
+                    }
+                  },
+                ),
+              ),
+            ],
+          )
       ),
     );
   }
