80db4bcfdc779d2651dc2c8236d15ef3ba16ca72
Zoya Belova (macbook) <alirasirin@gmail.com>
2023-10-02 17:22:07 +0300

    Добавила проверку на включенные источники в репозитории

--- lib/data/repository.dart
+++ lib/data/repository.dart
@@ -6,7 +6,7 @@ import 'datasource/vk_datasource.dart';
 import 'models/feed_model.dart';
 import 'models/post_model.dart';
 
-class PostsRepository extends ChangeNotifier{
+class PostsRepository extends ChangeNotifier {
   final RedditDataSource _redditDataSource;
   final VKDataSource _vkDataSource;
   double progress = 0.0;
@@ -16,36 +16,46 @@ class PostsRepository extends ChangeNotifier{
   PostsRepository(this._redditDataSource, this._vkDataSource);
 
   Future<List<Feed>> getAllFeeds() async {
-    //print('getAllFeeds');
     progress = 0;
     oldProgress = 0.0;
     List<Feed> feeds = [];
     Feed homeFeed = Feed(posts: [], keyword: 'All');
     final List<String> keywords = UserPreferences().getKeywords();
-    var progressSteps = keywords.length * 2;
-    //TODO брать включенные источники из UserPreferences
+    Map<String, bool> activeSources = {
+      'Reddit': UserPreferences().getRedditActive(),
+      'VK': UserPreferences().getVKActive(),
+      'Telegram': UserPreferences().getTelegramActive(),
+      'Youtube': UserPreferences().getYoutubeActive(),
+      'Instagram': UserPreferences().getInstagramActive(),
+    };
+
+    var progressSteps = keywords.length * activeSources.values.where((element) => element).length;
+
     for (var keyword in keywords) {
-      List<Post> _redditPosts = await _redditDataSource.getByKeyword(keyword);
-      progress += 1/progressSteps;
-      if (oldProgress != 0) {
-        oldProgress += 1/progressSteps;
+      List<Post> _allPosts = [];
+      if (activeSources['Reddit']!) {
+        List<Post> _redditPosts = await _redditDataSource.getByKeyword(keyword);
+        oldProgress = progress;
+        progress += 1 / progressSteps;
+        notifyListeners();
+        _allPosts.addAll(_redditPosts);
+        homeFeed.posts.addAll(_redditPosts);
       }
-      notifyListeners();
-      List<Post> _vkPosts = await _vkDataSource.getByKeyword(keyword);
-      progress += 1/progressSteps;
-      oldProgress += 1/progressSteps;
-      notifyListeners();
-     // print('reddit: ${_redditPosts.length} vk: ${_vkPosts.length}');
+      if (activeSources['VK']!) {
+        List<Post> _vkPosts = await _vkDataSource.getByKeyword(keyword);
+        oldProgress = progress;
+        progress += 1 / progressSteps;
+        notifyListeners();
+        _allPosts.addAll(_vkPosts);
+        homeFeed.posts.addAll(_vkPosts);
+      }
+
       //telegram
       //youtube
       //instagram
 
-      List<Post> _allPosts = [];
-      _allPosts.addAll(_redditPosts);
-      _allPosts.addAll(_vkPosts);
       _allPosts.sort((a, b) => b.date.compareTo(a.date));
       feeds.add(Feed(keyword: keyword, posts: _allPosts));
-      homeFeed.posts.addAll(_allPosts);
     }
     homeFeed.posts.sort((a, b) => b.date.compareTo(a.date));
     feeds.insert(0, homeFeed);
@@ -53,5 +63,4 @@ class PostsRepository extends ChangeNotifier{
     notifyListeners();
     return feeds;
   }
-
 }
