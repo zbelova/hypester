13d42795a522c0d5726396f85bdb7c3f0dae751c
Zoya Belova (macbook) <alirasirin@gmail.com>
2023-10-04 20:40:20 +0300

    Заполнила боковое меню

--- lib/presentation/home_page.dart
+++ lib/presentation/home_page.dart
@@ -1,4 +1,3 @@
-import 'package:draw/draw.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_login_vk/flutter_login_vk.dart';
@@ -6,10 +5,8 @@ import 'package:flutter_slider_drawer/flutter_slider_drawer.dart';
 import 'package:get_it/get_it.dart';
 import 'package:hypester/bloc/homepage/homepage_bloc.dart';
 import 'package:hypester/data/hive/feed_filters_local_data_source.dart';
-import 'package:hypester/data/repository.dart';
 import 'package:hypester/data/user_preferences.dart';
 import 'package:hypester/presentation/add_feed_screen.dart';
-import 'package:hypester/presentation/webview.dart';
 import 'package:hypester/presentation/widgets/progress_bar.dart';
 import '../bloc/homepage/homepage_event.dart';
 import '../bloc/homepage/homepage_state.dart';
@@ -109,9 +106,8 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
           title: const Text('Hypester', style: TextStyle(fontSize: 30, fontWeight: FontWeight.w600, fontFamily: 'Caveat-Variable')),
         ),
         key: const ValueKey('drawer'),
-        slider: Container(color: Colors.red),
+        slider: _buildSliderMenu(context),
         child: Column(
-
           children: [
             SizedBox(
               height: 35,
@@ -149,6 +145,14 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
     _tabController = TabController(vsync: this, length: state.feeds.length);
     return Scaffold(
       extendBody: true,
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.orangeAccent,
+        onPressed: () {
+          context.read<HomePageBloc>().add(LoadHomePageEvent());
+        },
+        child: const Icon(Icons.refresh),
+      ),
       body: SliderDrawer(
         appBar: SliderAppBar(
           appBarHeight: 100,
@@ -166,7 +170,7 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
           title: const Text('Hypester', style: TextStyle(fontSize: 30, fontWeight: FontWeight.w600, fontFamily: 'Caveat-Variable')),
         ),
         key: const ValueKey('drawer'),
-        slider: Container(color: Colors.red),
+        slider: _buildSliderMenu(context),
         child: Column(
           children: [
             SizedBox(
@@ -198,48 +202,21 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
                 children: [
                   for (var feed in state.feeds)
                     Padding(
-                      padding: const EdgeInsets.all(16),
+                      padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 14),
                       child: FeedScreen(feed: feed),
                     ),
                 ],
               ),
             ),
-            _token == null
-                ? Container(
-                    color: Colors.grey[100],
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      onPressed: loginVK,
-                      child: Text('Login to VK'),
-                    ),
-                  )
-                : Container(
-                    color: Colors.grey[200],
-                    width: double.infinity,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Text('You are logged in to VK'),
-                        ElevatedButton(
-                          onPressed: loginVK,
-                          child: Text('Logout'),
-                        ),
-                      ],
-                    ),
-                  ),
-            ElevatedButton(
-                onPressed: () {
-                  context.read<HomePageBloc>().add(LoadHomePageEvent());
-                },
-                child: Text('Refresh')),
-            SizedBox(height: 5),
-            ElevatedButton(
-                onPressed: () {
-                  FeedFiltersLocalDataSource.clear();
-                  UserPreferences().setKeywords([]);
-                },
-                child: Text('Clear Feeds')),
-            SizedBox(height: 20),
+
+            // SizedBox(height: 5),
+            // ElevatedButton(
+            //     onPressed: () {
+            //       FeedFiltersLocalDataSource.clear();
+            //       UserPreferences().setKeywords([]);
+            //     },
+            //     child: Text('Clear Feeds')),
+            // SizedBox(height: 20),
             // ElevatedButton(
             //     onPressed: () {
             //       Navigator.of(context).push(MaterialPageRoute(builder: (context) => const WebViewScreen()));
@@ -252,70 +229,199 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
     );
   }
 
-  Future<void> loginVK() async {
-    final token = _token;
-    final profile = _profile;
-    final isLogin = token != null;
-    await showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: Text('Login to VK'),
-            content: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                if (_sdkVersion != null) Text('SDK v$_sdkVersion'),
-                if (token != null && profile != null)
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 10),
-                    child: _buildUserInfo(context, profile, token, _email),
-                  ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    ElevatedButton(
-                      onPressed: () {
-                        _onPressedLogInButton(context);
-                      },
-                      child: Text('Login'),
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
-                        _onPressedLogOutButton();
-                      },
-                      child: Text('Logout'),
-                    )
-                  ],
+  Widget _buildSliderMenu(BuildContext context) {
+    bool _redditActive = UserPreferences().getRedditActive();
+    bool _vkActive = UserPreferences().getVKActive();
+    bool _youtubeActive = UserPreferences().getYoutubeActive();
+    bool _telegramActive = UserPreferences().getTelegramActive();
+    bool _instagramActive = UserPreferences().getInstagramActive();
+    return Container(
+      color: Colors.orangeAccent,
+      child: Padding(
+        padding: const EdgeInsets.only(top: 100, left: 40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                  width: 110,
+                  child: Text('Reddit',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.grey[800],
+                      )),
+                ),
+                Switch(
+                  activeColor: Colors.white,
+                  value: _redditActive,
+                  onChanged: (value) {
+                    setState(() {
+                      _redditActive = value;
+                    });
+                    UserPreferences().setRedditActive(_redditActive);
+                  },
                 ),
               ],
             ),
-          );
-        });
-  }
+            const SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                  width: 110,
+                  child: Text('Youtube',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.grey[800],
+                      )),
+                ),
+                Switch(
+                  activeColor: Colors.white,
+                  value: _youtubeActive,
+                  onChanged: (value) {
+                    setState(() {
+                      _youtubeActive = value;
+                    });
+                    UserPreferences().setYoutubeActive(_youtubeActive);
+                  },
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                  width: 110,
+                  child: Text('Telegram',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.grey[800],
+                      )),
+                ),
+                Switch(
+                  activeColor: Colors.white,
+                  value: _telegramActive,
+                  onChanged: (value) {
+                    setState(() {
+                      _telegramActive = value;
+                    });
+                    UserPreferences().setTelegramActive(_telegramActive);
+                  },
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                  width: 110,
+                  child: Text('VK',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.grey[800],
+                      )),
+                ),
+                Switch(
+                  activeColor: Colors.white,
+                  value: _vkActive,
+                  onChanged: _token != null
+                      ? (value) {
+                          setState(() {
+                            _vkActive = value;
+                          });
+                          UserPreferences().setVKActive(_vkActive);
+                        }
+                      : null,
+                ),
+              ],
+            ),
+            _token != null
+                ? InkWell(
+                    onTap: _onPressedLogOutButton,
+                    child: Text(
+                      'Sign out',
+                      style: TextStyle(color: Colors.grey[700]),
+                    ),
+                  )
+                : InkWell(
+                    onTap: () {
+                      _onPressedLogInButton(context);
+                    },
+                    child: Text('Sign in', style: TextStyle(color: Colors.grey[700])),
+                  ),
+            const SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                  width: 110,
+                  child: Text('Instagram',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.grey[800],
+                      )),
+                ),
+                Switch(
+                  activeColor: Colors.white,
+                  value: _instagramActive,
+                  onChanged: null,
+                  //     (value) {
+                  //   setState(() {
+                  //     _instagramActive = value;
+                  //   });
+                  //   UserPreferences().setInstagramActive(_instagramActive);
+                  // },
+                ),
+              ],
+            ),
+            Text('Sign in', style: TextStyle(color: Colors.grey[700])),
+            SizedBox(height: 60),
+            InkWell(
+              child: Row(
+                children: [
+                  Icon(Icons.search, color: Colors.grey[800], size: 25),
+                  SizedBox(width: 10),
+                  Text(
+                    'Feed filters',
+                    style: TextStyle(color: Colors.grey[800], fontSize: 20),
 
-  Widget _buildUserInfo(BuildContext context, VKUserProfile profile, VKAccessToken accessToken, String? email) {
-    final photoUrl = profile.photo200;
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        const Text('User: '),
-        Text(
-          '${profile.firstName} ${profile.lastName}',
-          style: const TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+              onTap: (){},
+            ),
+            SizedBox(height: 20),
+            InkWell(
+              child: Row(
+                children: [
+                  Icon(Icons.settings, color: Colors.grey[800], size: 25),
+                  SizedBox(width: 10),
+                  Text(
+                    'Settings',
+                    style: TextStyle(color: Colors.grey[800], fontSize: 20),
+
+                  ),
+                ],
+              ),
+              onTap: (){},
+            ),
+            Spacer(),
+          ],
         ),
-        Text(
-          'Online: ${profile.online}, Online mobile: ${profile.onlineMobile}',
-          style: const TextStyle(fontWeight: FontWeight.bold),
-        ),
-        if (photoUrl != null) Image.network(photoUrl),
-        const Text('AccessToken: '),
-        Text(
-          accessToken.token,
-          softWrap: true,
-        ),
-        Text('Created: ${accessToken.created}'),
-        if (email != null) Text('Email: $email'),
-      ],
+      ),
     );
   }
 
@@ -333,13 +439,12 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
       final loginResult = res.asValue!.value;
 
       if (!loginResult.isCanceled) await _updateLoginInfo();
-      Navigator.of(context).pop();
+      //Navigator.of(context).pop();
     }
   }
 
   Future<void> _onPressedLogOutButton() async {
     widget.plugin.logOut();
     await _updateLoginInfo();
-    Navigator.of(context).pop();
   }
 }
