04db9c68a4128796906490c77543285981c83e1b
Zoya Belova (macbook) <alirasirin@gmail.com>
2023-10-13 16:11:24 +0300

    Actions в webview

--- lib/presentation/webview.dart
+++ lib/presentation/webview.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hypester/presentation/widgets/youtube_video.dart';
+import 'package:share_plus/share_plus.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'package:webview_flutter_android/webview_flutter_android.dart';
 
@@ -25,7 +26,7 @@ class _WebViewScreenState extends State<WebViewScreen> {
   @override
   void initState() {
     super.initState();
-    if ( (widget.post.relinkUrl!.startsWith('https://www.youtube.com/') || widget.post.relinkUrl!.startsWith('https://youtu.be/'))) _isYoutube = true;
+    if ((widget.post.relinkUrl!.startsWith('https://www.youtube.com/') || widget.post.relinkUrl!.startsWith('https://youtu.be/'))) _isYoutube = true;
     if (!_isYoutube) {
       // #docregion platform_features
       late final PlatformWebViewControllerCreationParams params;
@@ -88,7 +89,34 @@ class _WebViewScreenState extends State<WebViewScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(widget.post.title != null ? widget.post.title! : '')),
+      appBar: AppBar(
+        title: Text(widget.post.title != null ? widget.post.title! : ''),
+        actions: <Widget>[
+          PopupMenuButton<String>(
+            onSelected: (value) {
+              _handleClick(value, context);
+            },
+            elevation: 1,
+            itemBuilder: (BuildContext context) {
+              return {'Share', 'Report'}.map((String choice) {
+                return PopupMenuItem<String>(
+                  value: choice,
+                  child: Row(
+                    children: [
+                      Icon(
+                        choice == 'Share' ? Icons.ios_share : Icons.report_outlined,
+                        size: 20,
+                      ),
+                      SizedBox(width: 10),
+                      Text(choice),
+                    ],
+                  ),
+                );
+              }).toList();
+            },
+          ),
+        ],
+      ),
       body: _showLoading && !_isYoutube
           ? const Center(
               child: CircularProgressIndicator(
@@ -103,6 +131,49 @@ class _WebViewScreenState extends State<WebViewScreen> {
       return WebViewWidget(controller: _controller);
     } else {
       return YoutubeVideo(videoUrl: widget.post.relinkUrl!, post: widget.post);
+    }
+  }
+
+  void _handleClick(String value, BuildContext context) {
+    switch (value) {
+      case 'Report':
+        showGeneralDialog(context: context,
+            pageBuilder: (context, anim1, anim2) => Container(),
+            barrierColor: Colors.black.withOpacity(0.5),
+            barrierDismissible: true,
+            barrierLabel: '',
+            transitionDuration: const Duration(milliseconds: 200),
+            transitionBuilder: (context, anim1, anim2, child) {
+              return Transform.scale(
+                scale: anim1.value,
+                child: Opacity(
+                  opacity: anim1.value,
+                  child: AlertDialog(
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                    title: Text('Report'),
+                    content: Text('Are you sure you want to report this post?'),
+                    actions: [
+                      TextButton(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        child: Text('Cancel'),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        child: Text('Report'),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            });
+        break;
+      case 'Share':
+        Share.share(widget.post.linkToOriginal!);
+        break;
     }
   }
 }
